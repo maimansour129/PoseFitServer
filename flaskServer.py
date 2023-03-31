eb84187e8607ce244b892ba12c40ee74e8cf7ce0
Sema Ghith <semaghith11@gmail.com>
2023-03-31 20:05:48 +0200

    Integrate squat model with server

--- flaskServer.py
+++ flaskServer.py
@@ -2,35 +2,57 @@ import io
 from flask import Flask, jsonify, request
 import base64
 from PIL import Image
-import cv2
+#import cv2
 import numpy as np
 
-
 from aiModels.bicepCurl import recieve_frame
+#from aiModels.squat import recieve_frame
+#TODO:name of functions (recieve_frame) should be changed 
 
 api = Flask(__name__)
 
-
 @api.route('/bicepCurl', methods=['POST'])
-def process():
+def bicepCurl():
 
     encoded_data = request.json
     
     decodedData = base64.b64decode(encoded_data)
 
     decodedImage = base64.decodebytes(decodedData)
-
+   
     image = Image.open(io.BytesIO(decodedImage))
-
+   
     img = np.asarray(image)
 
     #x to show returned image
     x = recieve_frame(img)
 
-    cv2.imwrite('try4.png', x)
+    #cv2.imwrite('try4.png', x)
 
     #TODO: return needed response
+    #return jsonify({'reps': counter, 'correction': feedback})      //model not edited yet
     return jsonify({'msg': 'success'})
+
+
+@api.route('/squat', methods=['POST'])
+def squat():
+
+    encoded_data = request.json
+    
+    decodedData = base64.b64decode(encoded_data)
+
+    decoded_image_data = base64.decodebytes(decodedData)
+
+    image = Image.open(io.BytesIO(decoded_image_data))
+
+    img = np.asarray(image)
+
+    x, counter, feedback = recieve_frame(img)
+
+    #cv2.imwrite('try4.png', x)
+
+    return jsonify({'reps': counter, 'correction': feedback})
+
 
 if __name__ == '__main__':
     api.run(port=5000, debug=True)
--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require("express");
 const app = express();
 const userRouter = require("./routes/userRoutes");
 const workoutRouter = require("./routes/workoutRoutes");
+const modelRouter=require("./routes/modelRoutes");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const mongoose = require("mongoose");
-const axios = require("axios");
 
 const dbURI =
   "mongodb+srv://PoseFit:PoseFit@cluster.y1yvcw2.mongodb.net/PoseFit?retryWrites=true&w=majority";
@@ -23,18 +23,4 @@ app.use(cookieParser());
 // routes
 app.use("/api/user", userRouter);
 app.use("/api/workout", workoutRouter);
-
-app.post("/model", (req, res) => {
-  console.log("hiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii  "+req.body.image)
-  const imageFile = req.body;
-  const imageStr = Buffer.from(imageFile.image).toString("base64");
-  
-
-  axios
-    .post("http://127.0.0.1:5000/bicepCurl", imageStr, {
-      headers: { "Content-Type": "application/json; charset=UTF-8" },
-    })
-    .then((response) => res.json(response.data))
-    .catch((err) => res.send(err));
-  console.log(res.body);
-});
+app.use("/api/model", modelRouter);
