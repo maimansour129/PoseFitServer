bb9f7bc8626251f48bac338c5889502ae784abb5
Sema Ghith <semaghith11@gmail.com>
2023-04-08 11:56:30 +0200

    Fix Bicep Curl Model Correction Issue

--- controllers/modelController.js
+++ controllers/modelController.js
@@ -1,5 +1,4 @@
 const axios = require("axios");
-const { response } = require("express");
 
 const bicepCurl = async (req, res) => {
   const imageFile = req.body;
@@ -9,7 +8,12 @@ const bicepCurl = async (req, res) => {
     .post("http://127.0.0.1:5000/bicepCurl", imageStr, {
       headers: { "Content-Type": "application/json; charset=UTF-8" },
     })
-    .then((response) => res.send(response.data))
+    .then((response) => {
+      res.send({
+        reps: response.data.reps,
+        correction: response.data.correction,
+      });
+    })
     .catch((err) => res.send(err));
 };
 
@@ -21,10 +25,12 @@ const squat = async (req, res) => {
     .post("http://127.0.0.1:5000/squat", imageStr, {
       headers: { "Content-Type": "application/json; charset=UTF-8" },
     })
-    .then((response) =>{
-      res.send({'reps':response.data.reps,'correction': response.data.correction})
-    }
-    )
+    .then((response) => {
+      res.send({
+        reps: response.data.reps,
+        correction: response.data.correction,
+      });
+    })
     .catch((err) => res.send(err));
 };
 
--- server.js
+++ server.js
@@ -7,7 +7,6 @@ const {requireAuth, checkUser} = require("./middleware/authMiddleware")
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const mongoose = require("mongoose");
-const axios = require("axios");
 const authRouter = require("./routes/authRoutes");
 const modelRouter=require("./routes/modelRoutes");
 
@@ -16,41 +15,23 @@ const dbURI =
 mongoose
   .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(app.listen(3000))
-  .then(console.log("Connected to mongodb, Listening on port 3000"))
   .catch((err) => console.log(err));
 
 // Middleware
+app.use(bodyParser.json({ limit: "50mb" }));
 app.use(express.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cookieParser());
+//app.use(cookieParser());
 
-app.use((req, res, next) => {
-  console.log(req.path, req.method);
-  next();
-});
+// app.use((req, res, next) => {
+//   console.log(req.path, req.method);
+//   next();
+// });
 
 // routes
-app.use("*", checkUser);
-app.use("/api/user", requireAuth, userRouter);
-
+//app.use("*", checkUser);
 app.use("/api/workout", requireAuth, workoutRouter);
-app.use("/api/auth",authRouter);
-
-app.post("/model", (req, res) => {
-  console.log("hiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii  "+req.body.image)
-  const imageFile = req.body;
-  const imageStr = Buffer.from(imageFile.image).toString("base64");
-  
-
-  axios
-    .post("http://127.0.0.1:5000/bicepCurl", imageStr, {
-      headers: { "Content-Type": "application/json; charset=UTF-8" },
-    })
-    .then((response) => res.json(response.data))
-    .catch((err) => res.send(err));
-  console.log(res.body);
-});
-
+app.use("/api/user", requireAuth, userRouter);
 app.use("/api/workout", workoutRouter);
 app.use("/api/model", modelRouter);
+app.use("/api/auth",authRouter);
 
